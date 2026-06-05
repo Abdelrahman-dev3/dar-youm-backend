419b7096314d75ca14fd2238f810dcc4deda72bd
Anwar Eisa <anwar.eisa94@gmail.com>
2026-06-05 21:39:12 +0300

    add project

--- config/cors.php
+++ config/cors.php
@@ -4,12 +4,17 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
+        'https://daryum.com',
+        'https://www.daryum.com',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
     ],
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^http://localhost(:[0-9]+)?$#',
+        '#^http://127\.0\.0\.1(:[0-9]+)?$#',
+    ],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
